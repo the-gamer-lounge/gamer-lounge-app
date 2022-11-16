5d3f25a0a0bf093696d87d4e000f9d351d057de3
rhodesmt <mrhodes415@gmail.com>
2022-11-16 13:33:58 -0800

    Updated Seed data and conditional rendering for show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 user1 = User.where(email: "test1@example.com",  username: "CODGOD").first_or_create(password: "password", password_confirmation: "password")
-user2 = User.where(email: "test2@example.com", username: "GitGüd").first_or_create(password: "password", password_confirmation: "password")
+user2 = User.where(email: "techlead@example.com", username: "techlead").first_or_create(password: "password", password_confirmation: "password")
 
 game1 = Game.create(
   image: 'https://www.callofduty.com/content/dam/atvi/callofduty/cod-touchui/blog/hero/mwii/MWII-000-ARTREVEAL-TOUT.jpg',
