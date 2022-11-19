f4f840d3815662af9f9b1767d5ba7f108c564e71
rhodesmt <mrhodes415@gmail.com>
2022-11-18 16:17:53 -0800

    Added validations for the review difficulty and rating, updated other tests to reflect the updated review model data.

--- spec/requests/reviews_spec.rb
+++ spec/requests/reviews_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "Reviews", type: :request do
-  let(:user) { User.create email: "test@example.com", password: "password", password_confirmation: "password" }
+  let(:user) { User.create email: "test@example.com", password: "password", password_confirmation: "password", username: "user" }
   let(:game) { Game.create(
     image: 'https://www.callofduty.com/content/dam/atvi/callofduty/cod-touchui/blog/hero/mwii/MWII-000-ARTREVEAL-TOUT.jpg',
     title: 'Call of Duty®: Modern Warfare® II',
@@ -17,10 +17,11 @@ RSpec.describe "Reviews", type: :request do
     it "gets all the reviews" do
       review = user.reviews.create(
         game_id: game.id,
-        review: "Starting with the campaign it was 17 missions long. Overall the missions were great, there were a few that seemed a little too long. The AC-130 mission for example took a little too long, but that's a preference as I prefer to play the boots on the ground missions. There is a mission towards the end that you have to sneak around and while sometimes it got confusing and you die quite a few times you get the hang of it and it becomes thriving. The graphics were great throughout the whole game and the design was very thought out. Moving onto the multiplayer, the maps are lacking... They don't give the same vibe as the previous OG modern warfare. However, the guns, and the weapon tuning are new and exciting. The weapon tuning gives the game a whole new dimension. I hope this gives Warzone a new look. Overall, I'm very happy with the happy thus-far and I look forward to what the future holds.",
+        review_text: "Starting with the campaign it was 17 missions long. Overall the missions were great, there were a few that seemed a little too long. The AC-130 mission for example took a little too long, but that's a preference as I prefer to play the boots on the ground missions. There is a mission towards the end that you have to sneak around and while sometimes it got confusing and you die quite a few times you get the hang of it and it becomes thriving. The graphics were great throughout the whole game and the design was very thought out. Moving onto the multiplayer, the maps are lacking... They don't give the same vibe as the previous OG modern warfare. However, the guns, and the weapon tuning are new and exciting. The weapon tuning gives the game a whole new dimension. I hope this gives Warzone a new look. Overall, I'm very happy with the happy thus-far and I look forward to what the future holds.",
         accessibility: "Yes",
         difficulty: 4,
-        rating: 4.0
+        rating: 4.0,
+        username: "user"
       )
       review.save
       get "/reviews"
@@ -32,6 +33,7 @@ RSpec.describe "Reviews", type: :request do
       expect(review["accessibility"]).to eq "Yes"
       expect(review["difficulty"]).to eq 4
       expect(review["rating"]).to eq 4.0
+      expect(review["username"]).to eq "user"
     end
   end
 
@@ -39,18 +41,20 @@ RSpec.describe "Reviews", type: :request do
     it "shows a single review" do
       newreview1 = Review.create(
         game_id: game.id,
-        review: "Starting with the campaign it was 17 missions long. Overall the missions were great, there were a few that seemed a little too long. The AC-130 mission for example took a little too long, but that's a preference as I prefer to play the boots on the ground missions. There is a mission towards the end that you have to sneak around and while sometimes it got confusing and you die quite a few times you get the hang of it and it becomes thriving. The graphics were great throughout the whole game and the design was very thought out. Moving onto the multiplayer, the maps are lacking... They don't give the same vibe as the previous OG modern warfare. However, the guns, and the weapon tuning are new and exciting. The weapon tuning gives the game a whole new dimension. I hope this gives Warzone a new look. Overall, I'm very happy with the happy thus-far and I look forward to what the future holds.",
+        review_text: "Starting with the campaign it was 17 missions long. Overall the missions were great, there were a few that seemed a little too long. The AC-130 mission for example took a little too long, but that's a preference as I prefer to play the boots on the ground missions. There is a mission towards the end that you have to sneak around and while sometimes it got confusing and you die quite a few times you get the hang of it and it becomes thriving. The graphics were great throughout the whole game and the design was very thought out. Moving onto the multiplayer, the maps are lacking... They don't give the same vibe as the previous OG modern warfare. However, the guns, and the weapon tuning are new and exciting. The weapon tuning gives the game a whole new dimension. I hope this gives Warzone a new look. Overall, I'm very happy with the happy thus-far and I look forward to what the future holds.",
         accessibility: "Yes",
         difficulty: 4,
-        rating: 4.0
+        rating: 4.0,
+        username: "user"
       )
 
       newreview2 = Review.create(
         game_id: game.id,
-        review: "Fortnite is an excellent game that is always changing. The developers realize that games need change and substance to go with it. Fortnite has been out for years and is still one of the best rated and played games out there. While its evolved over the years and has had it's ups and downs nobody can take away the fact the developers have gone above and beyond with the constant updates. Building is a thing of the past since the zero build mode has come out. It's brought new gamers to Fortnite who hated the building aspect and has been proven to be the new identity to Fortnite (atleast for me, I won't touch the building modes anymore). Building made it incredibly hard for the average gamer to hop on and play. Creative has taken video games to a different outlook as well with the different modes and how people can simply create their own world. I can go on and on, but Fortnite as a whole is a phenomenal game that is worth checking out.",
+        review_text: "Fortnite is an excellent game that is always changing. The developers realize that games need change and substance to go with it. Fortnite has been out for years and is still one of the best rated and played games out there. While its evolved over the years and has had it's ups and downs nobody can take away the fact the developers have gone above and beyond with the constant updates. Building is a thing of the past since the zero build mode has come out. It's brought new gamers to Fortnite who hated the building aspect and has been proven to be the new identity to Fortnite (atleast for me, I won't touch the building modes anymore). Building made it incredibly hard for the average gamer to hop on and play. Creative has taken video games to a different outlook as well with the different modes and how people can simply create their own world. I can go on and on, but Fortnite as a whole is a phenomenal game that is worth checking out.",
         accessibility: "Yes",
         difficulty: 7,
-        rating: 4.7
+        rating: 4.7,
+        username: "user"
       )
         get "/reviews/#{newreview2.id}"
         expect(response).to have_http_status(200)
@@ -63,10 +67,11 @@ describe "POST /create" do
       review: {
         game_id: game.id,
         user_id: user.id,
-        review: "Fortnite is an excellent game that is always changing. The developers realize that games need change and substance to go with it. Fortnite has been out for years and is still one of the best rated and played games out there. While its evolved over the years and has had it's ups and downs nobody can take away the fact the developers have gone above and beyond with the constant updates. Building is a thing of the past since the zero build mode has come out. It's brought new gamers to Fortnite who hated the building aspect and has been proven to be the new identity to Fortnite (atleast for me, I won't touch the building modes anymore). Building made it incredibly hard for the average gamer to hop on and play. Creative has taken video games to a different outlook as well with the different modes and how people can simply create their own world. I can go on and on, but Fortnite as a whole is a phenomenal game that is worth checking out.",
+        review_text: "Fortnite is an excellent game that is always changing. The developers realize that games need change and substance to go with it. Fortnite has been out for years and is still one of the best rated and played games out there. While its evolved over the years and has had it's ups and downs nobody can take away the fact the developers have gone above and beyond with the constant updates. Building is a thing of the past since the zero build mode has come out. It's brought new gamers to Fortnite who hated the building aspect and has been proven to be the new identity to Fortnite (atleast for me, I won't touch the building modes anymore). Building made it incredibly hard for the average gamer to hop on and play. Creative has taken video games to a different outlook as well with the different modes and how people can simply create their own world. I can go on and on, but Fortnite as a whole is a phenomenal game that is worth checking out.",
         accessibility: "Yes",
         difficulty: 7,
-        rating: 4.7
+        rating: 4.7,
+        username: "user"
       }
     }
 
@@ -83,10 +88,11 @@ describe "POST /create" do
         review: {
           game_id: game.id,
           user_id: user.id,
-          review: "Fortnite is an excellent game that is always changing. The developers realize that games need change and substance to go with it. Fortnite has been out for years and is still one of the best rated and played games out there. While its evolved over the years and has had it's ups and downs nobody can take away the fact the developers have gone above and beyond with the constant updates. Building is a thing of the past since the zero build mode has come out. It's brought new gamers to Fortnite who hated the building aspect and has been proven to be the new identity to Fortnite (atleast for me, I won't touch the building modes anymore). Building made it incredibly hard for the average gamer to hop on and play. Creative has taken video games to a different outlook as well with the different modes and how people can simply create their own world. I can go on and on, but Fortnite as a whole is a phenomenal game that is worth checking out.",
+          review_text: "Fortnite is an excellent game that is always changing. The developers realize that games need change and substance to go with it. Fortnite has been out for years and is still one of the best rated and played games out there. While its evolved over the years and has had it's ups and downs nobody can take away the fact the developers have gone above and beyond with the constant updates. Building is a thing of the past since the zero build mode has come out. It's brought new gamers to Fortnite who hated the building aspect and has been proven to be the new identity to Fortnite (atleast for me, I won't touch the building modes anymore). Building made it incredibly hard for the average gamer to hop on and play. Creative has taken video games to a different outlook as well with the different modes and how people can simply create their own world. I can go on and on, but Fortnite as a whole is a phenomenal game that is worth checking out.",
           accessibility: "Yes",
           difficulty: 7,
-          rating: 4.7
+          rating: 4.7,
+          username: "user"
         }
       }
 
@@ -98,10 +104,11 @@ describe "POST /create" do
         review:  {
           game_id: game.id,
           user_id: user.id,
-          review: "Fortnite is an excellent game that is always changing. The developers realize that games need change and substance to go with it. Fortnite has been out for years and is still one of the best rated and played games out there. While its evolved over the years and has had it's ups and downs nobody can take away the fact the developers have gone above and beyond with the constant updates. Building is a thing of the past since the zero build mode has come out. It's brought new gamers to Fortnite who hated the building aspect and has been proven to be the new identity to Fortnite (atleast for me, I won't touch the building modes anymore). Building made it incredibly hard for the average gamer to hop on and play. Creative has taken video games to a different outlook as well with the different modes and how people can simply create their own world. I can go on and on, but Fortnite as a whole is a phenomenal game that is worth checking out.",
+          review_text: "Fortnite is an excellent game that is always changing. The developers realize that games need change and substance to go with it. Fortnite has been out for years and is still one of the best rated and played games out there. While its evolved over the years and has had it's ups and downs nobody can take away the fact the developers have gone above and beyond with the constant updates. Building is a thing of the past since the zero build mode has come out. It's brought new gamers to Fortnite who hated the building aspect and has been proven to be the new identity to Fortnite (atleast for me, I won't touch the building modes anymore). Building made it incredibly hard for the average gamer to hop on and play. Creative has taken video games to a different outlook as well with the different modes and how people can simply create their own world. I can go on and on, but Fortnite as a whole is a phenomenal game that is worth checking out.",
           accessibility: "Yes",
           difficulty: 7,
-          rating: 4.5
+          rating: 4.5,
+          username: "user"
         }
       }
 
@@ -118,10 +125,11 @@ describe "POST /create" do
         review: {
           game_id: game.id,
           user_id: user.id,
-          review: "Fortnite is an excellent game that is always changing. The developers realize that games need change and substance to go with it. Fortnite has been out for years and is still one of the best rated and played games out there. While its evolved over the years and has had it's ups and downs nobody can take away the fact the developers have gone above and beyond with the constant updates. Building is a thing of the past since the zero build mode has come out. It's brought new gamers to Fortnite who hated the building aspect and has been proven to be the new identity to Fortnite (atleast for me, I won't touch the building modes anymore). Building made it incredibly hard for the average gamer to hop on and play. Creative has taken video games to a different outlook as well with the different modes and how people can simply create their own world. I can go on and on, but Fortnite as a whole is a phenomenal game that is worth checking out.",
+          review_text: "Fortnite is an excellent game that is always changing. The developers realize that games need change and substance to go with it. Fortnite has been out for years and is still one of the best rated and played games out there. While its evolved over the years and has had it's ups and downs nobody can take away the fact the developers have gone above and beyond with the constant updates. Building is a thing of the past since the zero build mode has come out. It's brought new gamers to Fortnite who hated the building aspect and has been proven to be the new identity to Fortnite (atleast for me, I won't touch the building modes anymore). Building made it incredibly hard for the average gamer to hop on and play. Creative has taken video games to a different outlook as well with the different modes and how people can simply create their own world. I can go on and on, but Fortnite as a whole is a phenomenal game that is worth checking out.",
           accessibility: "Yes",
           difficulty: 7,
-          rating: 4.5
+          rating: 4.5,
+          username: "user"
         }
       }
 
